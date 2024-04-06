b1e3942d66982ed9fec07063e8453626e72dbf78
Michael Hueschen <m@mhueschen.dev>
2024-04-06 10:05:27 -0600

    flakify

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.plankShell = import ./shell.nix { inherit pkgs; };
+
+        packages.plank = import ./default.nix { inherit pkgs; };
+      });
+}
