operating-function/plan-di-c

commit 1cacf46e1f6a5350891330a2ba2537e600848dba

remove extraneous quoting

test.sh
@@ -38,7 +38,7 @@ Dec="($MkPin ($MkLaw %Dec 1 (0 (0 ($NatCase (0 0)) $id) 1)))"
 ToNat="($NatCase 0 $Inc)"
 Times="($MkLaw %Times 3 (0 (0 (0 $NatCase 2) (0 (0 0 1) (0 1 2))) 3))"
 Add="($MkPin ($MkLaw %Add 2 (0 (0 ($Times $Inc) (0 $ToNat 1)) 2)))"
-Mul="($MkPin ($MkLaw %Mul 2 (0 (0 (0 $Times (0 $Add 1)) (0 0)) 2)))"
+Mul="($MkPin ($MkLaw %Mul 2 (0 (0 (0 $Times ($Add 1)) (0 0)) 2)))"
 Sub="($MkPin ($MkLaw %Sub 2 (0 (0 ($Times $Dec) (0 $ToNat 1)) 2)))"
 
 Ignore="($MkLaw 0 2 2)"
@@ -77,7 +77,7 @@ echo "basic"
 check "5" "($Inc 4)"
 check "1" "($Inc ($PlanCase 1 0 0 0 (4 9)))"
 check "7" "($Dec 8)"
-check "8" "(($MkLaw 1 2 (0 ($Inc 7))) 3 4)"
+check "8" "($MkLaw 1 2 ($Inc 7) 3 4)"
 check "{1 2 0}" "($MkLaw ($Inc 0) ($Inc ($Inc 0)) 0)"
 check "{1 2 0}" "(($MkLaw 1 2 0) 9 7)"
 check "9" "(($MkLaw 1 2 1) 9 7)"

commit 89f27dc04c229df9426624ac1839ddeb9280d65c

fix bug in Mul introduced in 1cacf46e

test.sh
@@ -38,7 +38,7 @@ Dec="($MkPin ($MkLaw %Dec 1 (0 (0 ($NatCase (0 0)) $id) 1)))"
 ToNat="($NatCase 0 $Inc)"
 Times="($MkLaw %Times 3 (0 (0 (0 $NatCase 2) (0 (0 0 1) (0 1 2))) 3))"
 Add="($MkPin ($MkLaw %Add 2 (0 (0 ($Times $Inc) (0 $ToNat 1)) 2)))"
-Mul="($MkPin ($MkLaw %Mul 2 (0 (0 (0 $Times ($Add 1)) (0 0)) 2)))"
+Mul="($MkPin ($MkLaw %Mul 2 (0 (0 (0 $Times (0 $Add 1)) (0 0)) 2)))"
 Sub="($MkPin ($MkLaw %Sub 2 (0 (0 ($Times $Dec) (0 $ToNat 1)) 2)))"
 
 LawDec="($MkLaw %Dec 1 (0 (0 ($NatCase (0 0)) $id) 1))"