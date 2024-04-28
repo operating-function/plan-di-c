7677c83701b77184363b4f286bbed63875c351aa
Michael Hueschen <m@mhueschen.dev>
2024-04-28 11:38:17 -0600

    ide.sh

--- ide.sh
+++ ide.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p inotify-tools
 #
+
+setterm -linewrap off
+
 compile () {
   clear
-  make plan 2>&1 | head "-n$((`tput lines` - 12))"
+  make plan 2>&1 | head "-n$((`tput lines` - 6))"
 }
 
 compile
